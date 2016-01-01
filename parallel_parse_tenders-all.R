0a9a023a301dbb6ff86a511f2abb17806d630c85
Wush Wu <wush978@gmail.com>
2016-01-01 17:42:40 +0800

    write to tenders.Rds

--- parallel_parse_tenders-all.R
+++ parallel_parse_tenders-all.R
@@ -1,19 +1,11 @@
-suppressPackageStartupMessages({
-  library(pbdMPI)
-  library(logging)
-  source("bparser.R")
-  library(pbdMPILogging)
-  basicConfig()
-  convert_to_parallel_logger(getLogger())
-})
-.rank <- comm.rank()
-.size <- comm.size()
-retval.path <- sprintf("parallel_parse_tenders-%03d.Rds", .rank)
-loginfo(sprintf("Reading archive %s ...", retval.path))
-retval <- readRDS(retval.path)
-retval.all <- gather(retval, unlist = FALSE)
-finalize()
-if (.rank == 0) {
-  retval.all <- unlist(retval.all, recursive = FALSE)
-  saveRDS(retval.all, "parallel_parse_tenders-all.Rds")
+library(logging)
+basicConfig()
+retval <- list()
+for(.rank in 0:59) {
+  retval.path <- sprintf("parallel_parse_tenders-%03d.Rds", .rank)
+  loginfo(sprintf("Reading archive %s ...", retval.path))
+  retval[[.rank + 1]] <- readRDS(retval.path)
 }
+retval.all <- unlist(retval, recursive = FALSE)
+saveRDS(retval.all, "tenders.Rds")
+
